59b84b034027a58555884dcdbc674c26970d788b
adrian higuero <adrianhiguerotorres27@gmail.com>
2016-02-17 11:00:12 +0100

    I have added my new hello world

---
+++ Hello/Hello/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+	printf("WELCOME TO THE HELLO WORLD\n");
+	printf("Hello world\n");
+
+
+
+	printf("\t\t\t\t\t\t\tVersion 0.1\n");
+	system("pause");
+	return 0;
+
+
+}
